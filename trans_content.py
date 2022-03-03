31a015d37b6019d736299a7505c5a222972d8c8e
aadityayadav <67205038+aadityayadav@users.noreply.github.com>
2022-03-03 12:03:00 -0500

    Utilizing google_trans_new instead of google_trans

--- trans_content.py
+++ trans_content.py
@@ -1,24 +1,54 @@
 import googletrans
 from googletrans import * 
-# from googletrans import Translator
+from google_trans_new import google_translator
+import csv
 
+py_keys = []
 
-translator = googletrans.Translator()
+with open('python_keyword.csv', mode = 'r') as file:
+    csvFile = csv.reader(file)
+    fields = next(file)
+    for row in csvFile:
+        py_keys.append(row)
 
-key_file = open('python_keyword.txt', 'r')
-Lines = key_file.readlines()
 
 all_lang = googletrans.LANGUAGES
 
 
+# for adding to a csv file
+
 for key in all_lang:
     lang = all_lang[key]
-    name_file = lang + '_python_keyword.txt'
-    new_file = open(name_file,'w')
+    name_file = lang + '_python_keyword.csv'
 
-    for word in Lines:
-        py_key = word
-        translate = translator.translate(py_key, dest = lang)
-        new_file.write(translate.text)
+    with open(name_file, 'w') as new_file:
+        writer = csv.writer(new_file)
+        header_tag = "List of all Python Keywords in " + lang
+        header = [header_tag]
+        writer.writerow(header)
+
+        for word in py_keys:
+            py_key = word[0]
+            translator = google_translator()  
+            translate_text = translator.translate(py_key, lang_tgt = key)
+            translate_text_arr = []
+            translate_text_arr.append(translate_text)
+            writer.writerow(translate_text_arr)
+        
+
+
+#for adding to a txt file
+
+# for key in all_lang:
+#     lang = all_lang[key]
+#     name_file = lang + '_python_keyword.txt'
+#     new_file = open(name_file,'w')
+
+#     for word in py_keys:
+#         py_key = word[0]
+#         translator = google_translator()  
+#         translate_text = translator.translate(py_key, lang_tgt = key)
+#         new_file.write(translate_text)
+#         new_file.write("\n")
     
-    new_file.close()
+#     new_file.close()
