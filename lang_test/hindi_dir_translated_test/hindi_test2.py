90e53b82812d885fdeb874eed6bb6a4505495c20
AdityaSwarup <43361986+AdityaSwarup@users.noreply.github.com>
2025-01-05 12:30:57 -0500

    Test files added

---
+++ lang_test/hindi_dir_translated_test/hindi_test2.py
@@ -0,0 +1,8 @@
+from hindi_helper_module import स्वागत
+
+def मैन():
+    नाम = input("आपका नाम: ")
+    मेसिज = स्वागत(नाम)
+    print(मेसिज)
+if __name__ == "__main__":
+    मैन()
---
+++ lang_test/hindi_test_translated_run.py
@@ -0,0 +1,14 @@
+def मुख्य():
+    if 10 > 5 and 5 != 0:
+        print("सत्य")
+    else:
+        print("झूठा")
+for अ in [1, 2, 3]:
+    if अ == 2:
+        break
+    print(अ)
+try:
+    अ = 10 / 0
+except:
+    print("अपवाद उठा")
+मुख्य()
